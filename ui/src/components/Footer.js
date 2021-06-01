c833aa8844c148d17f1e23dfa870214b7623f564
Raymond Oluoch <rasholuoch@gmail.com>
2021-06-01 19:34:01 +0300

    adding style theme for ui

---
+++ ui/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <Footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>
+            Copyright &copy; All Rights Reserved | E-Shop
+          </Col>
+        </Row>
+      </Container>
+    </Footer>
+  )
+}
+
+export default Footer
